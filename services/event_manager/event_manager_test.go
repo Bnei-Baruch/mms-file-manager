44bebc587d2bf895ffe5b28e4d3222e7287e0bb6
ramigg <ramigg@gmail.com>
2015-10-31 16:41:09 +0200

    initial event manager

---
+++ services/event_manager/event_manager_test.go
@@ -0,0 +1,15 @@
+package event_manager_test
+
+import (
+	. "github.com/onsi/ginkgo"
+)
+
+var _ = Describe("EventManager", func() {
+	Describe("Creating new event", func() {
+		Context("When file name is valid", func() {
+		})
+
+		Context("When file name is invalid", func() {
+		})
+	})
+})
